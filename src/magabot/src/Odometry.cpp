0cb105e5d11a5ff89366c41636dc1bc3c134b7db
Rui Posse <rui.posse@vision247.com>
2014-01-28 16:09:19 +0000

    Replaced Magabot package by an improved one, and added imu package

--- src/magabot/src/Odometry.cpp
+++ src/magabot/src/Odometry.cpp
@@ -2,174 +2,152 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_broadcaster.h>
-#include "magabot/motorClick.h"
+#include "magabot/Status.h"
 
-#define nodeName "MotorOdometry" // Node name
+#define _USE_MATH_DEFINES
 
-#define pubName "odom" // Topic name to publish odometry information (nav_msgs::Odometry)
-#define pubQueue 1000 // Size of publisher topic queue
-#define pubRate 80 // Publishing Rate (Hz)
+#define NODE_NAME "Odometry"
 
-#define subName "arduinoTicks" // Topic name to subscribe tick information from arduino (ticks)
-#define subQueue 1000 // Size of subscriber topic queue
+#define PUB_NAME "odom"
+#define PUB_QUEUE_SIZE 1000
+#define PUB_RATE 100
 
-#define odomFrame_id "odom" // Odometry frame id
-#define odomChildFrame_id "base_link" // Odometry Child frame id
+#define SUB_NAME "/magabot/Status"
+#define SUB_QUEUE_SIZE 1000
 
-#define PI 3.14159
+#define ODOM_FRAME_ID "odom"
+#define ODOM_CHILD_FRAME_ID "base_link"
 
-#define Dist 0.355 // Distance between front wheels (meters)
-#define R 0.0926/2 // Radius of front wheels (meters)
-#define Rev 3900 // Encoder ticks in a wheel revolution (ticks)
+#define DISTANCE 0.345
+#define RADIUS 0.045
+#define TICKS_PER_REV 3900
 
-#define dt 0.1 // Time between position samples (seconds)
+#define K_TH 2 * M_PI * RADIUS / (DISTANCE * TICKS_PER_REV)
+#define K_XY RADIUS * M_PI / TICKS_PER_REV
 
-#define cte_th 2*PI*R/(Dist*Rev) // Mathematical conversion from ticks to radians
-#define cte_xy R*PI/Rev // Mathematical conversion from ticks to meters
-
-class Listener{
+class Listener
+{
 	private:
-		// Individual motor ticks variables
-		int leftMotorTicks;		
-		int rightMotorTicks;		
-		
-		// Geometry velocity variables
-		double vx;
-		double vy;
-		double wz;
+		int left_wheel_ticks;
+		int right_wheel_ticks;
 
-		// Distance traveled variables
+		double v_x;
+		double v_y;
+		double w_z;
+
 		double x;
 		double y;
 		double th;
-		
-		// Message to publish
-		nav_msgs::Odometry odom;
-		
-		// ROS time variable for odometry message
-		ros::Time scanTime;
-		
-		// Quaternion needed for Odometry
-    geometry_msgs::Quaternion odomQuaternion;
-		
-		// Topic variables
-		ros::Subscriber subscriber;
-  	ros::Publisher publisher;
 
-		// Topic functions
-		void publishMessage();
-		void callback(const magabot::motorClick::ConstPtr& msg);
+		double dt;
+
+		nav_msgs::Odometry odom;
+
+		ros::Time time_stamp;
+
+		geometry_msgs::Quaternion odom_quaternion;
+
+		ros::Subscriber subscriber;
+		ros::Publisher publisher;
+
+		void publish();
+		void callback(const magabot::Status::ConstPtr& msg);
 
 	public:
 		Listener();
-		
-		void init(int argc, char **argv);	
-  	void run();
+		void init(int argc, char **argv);
+		void run();
 };
 
-Listener::Listener(){
-	// Listener instantiation, reset the variables
-	leftMotorTicks = 0;		
-	rightMotorTicks = 0;		
-	
-	vx = 0.0;
-	vy = 0.0;
-	wz = 0.0;
+Listener::Listener()
+{
+	left_wheel_ticks = 0;
+	right_wheel_ticks = 0;
+
+	v_x = 0.0;
+	v_y = 0.0;
+	w_z = 0.0;
 
 	x = 0.0;
 	y = 0.0;
-	th = 0.0;
+	th = 0.0;  
+
+	dt = 0.0;
 }
 
-void Listener::init(int argc, char **argv){
-	// Initiate the ROS Node and NodeHandle
-	ros::init(argc, argv, nodeName);
-	ros::NodeHandle handle;
-	
-	// Initiate the subscriber and publisher
-	subscriber = handle.subscribe(subName, subQueue, &Listener::callback, this);
-	publisher = handle.advertise<nav_msgs::Odometry>(pubName, pubQueue);
+void Listener::init(int argc, char **argv)
+{
+	ros::init(argc, argv, NODE_NAME);
+	ros::NodeHandle nh;
+
+	subscriber = nh.subscribe(SUB_NAME, SUB_QUEUE_SIZE, &Listener::callback, this);
+	publisher = nh.advertise<nav_msgs::Odometry>(PUB_NAME, PUB_QUEUE_SIZE);
 }
 
-void Listener::callback(const magabot::motorClick::ConstPtr& msg){
-	// Update the ticks values from the subscribed topic
-	rightMotorTicks = msg->motorRight;
-	leftMotorTicks = msg->motorLeft;
-	
-	// Convert the individual ticks into position variation
-	double delta_th = cte_th * double(rightMotorTicks - leftMotorTicks);
-	double delta_x = cte_xy * double(rightMotorTicks + leftMotorTicks) * cos(th);
-	double delta_y = cte_xy * double(rightMotorTicks + leftMotorTicks) * sin(th);
-	
-	// Update the position estimates
+void Listener::callback(const magabot::Status::ConstPtr& msg)
+{
+	right_wheel_ticks = msg->encoders.right_wheel;
+	left_wheel_ticks = msg->encoders.left_wheel;
+	dt = msg->timestamp.toSec() - dt;
+
+	double delta_th = K_TH * double(right_wheel_ticks - left_wheel_ticks);
+	double delta_x = K_XY * double(right_wheel_ticks + left_wheel_ticks) * cos(th);
+	double delta_y = K_XY * double(right_wheel_ticks + left_wheel_ticks) * sin(th);
+
 	x += delta_x;
 	y += delta_y;
 	th += delta_th;
-	
-	// Update the current velocity estimates
-	vx = double(delta_x / dt);
-	vy = double(delta_y / dt);
-	wz = double(delta_th / dt);
-	odomQuaternion = tf::createQuaternionMsgFromYaw(th);
+
+	v_x = double(delta_x / dt);
+	v_y = double(delta_y / dt);
+	w_z = double(delta_th / dt);
+	odom_quaternion = tf::createQuaternionMsgFromYaw(th);
 }
 
-void Listener::publishMessage(){
-	// Build and publish the Odometry message to publish
-	
-	// Set the time
-	scanTime = ros::Time::now();
-	
-	// Set the header
-	odom.header.stamp = scanTime;
-  odom.header.frame_id = odomFrame_id;
+void Listener::publish()
+{
+	time_stamp = ros::Time::now();
 
-  // Set the position
-  odom.pose.pose.position.x = x;
- 	odom.pose.pose.position.y = y;
- 	odom.pose.pose.position.z = 0.0;
- 	odom.pose.pose.orientation = odomQuaternion;
+	odom.header.stamp = ros::Time::now();
+	odom.header.frame_id = ODOM_FRAME_ID;
 
-	// Set the velocity
-	odom.child_frame_id = odomChildFrame_id;
-	odom.twist.twist.linear.x = vx;
-	odom.twist.twist.linear.y = vy;
-	odom.twist.twist.angular.z = wz;
+	odom.pose.pose.position.x = x;
+	odom.pose.pose.position.y = y;
+	odom.pose.pose.orientation = odom_quaternion;
 
-	// Set the pose covariance
-	odom.pose.covariance[0]  = 0.001;
-	odom.pose.covariance[7]  = 0.001;
+	odom.child_frame_id = ODOM_CHILD_FRAME_ID;
+	odom.twist.twist.linear.x = v_x;
+	odom.twist.twist.linear.y = v_y;
+	odom.twist.twist.angular.z = w_z;
+
+	odom.pose.covariance[0] = 0.001;
+	odom.pose.covariance[7] = 0.001;
 	odom.pose.covariance[14] = 0.001;
 	odom.pose.covariance[21] = 1000;
 	odom.pose.covariance[28] = 1000;
 	odom.pose.covariance[35] = 1000;
-	
-	// Set the twist covariance
+
 	odom.twist.covariance = odom.pose.covariance;
 
 	publisher.publish(odom);
 }
 
-void Listener::run(){
-	ros::Rate loopRate(pubRate);
-	
-	while (ros::ok()){    
-		ros::spinOnce();
-		
-		publishMessage();
+void Listener::run()
+{
+	ros::Rate loopRate(PUB_RATE);
 
+	while (ros::ok()) {
+		ros::spinOnce();
+		publish();
 		loopRate.sleep();
 	}
 }
 
-// Main function
 int main(int argc, char **argv)
 {
 	Listener listener;
-
 	listener.init(argc, argv);
-
-  listener.run();
+	listener.run();
 
 	return 0;
 }
-
